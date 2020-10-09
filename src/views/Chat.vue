b84d0ceda8ed719a3f7c45e7a0d887d0be9d6d31
yvansabay <sabayyvan2018@gmail.com>
2020-10-09 09:56:51 +0800

    added room feature

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    <div class="container mt-5" style="min-height: 80vh; max-height: 80vh; overflow-y: auto">
+    <div class="container" v-if="roomNo == ''" style="height: 100vh">
+      <div class="row h-100 justify-content-center align-items-center">
+        <div class="col-9 col-sm-8 col-md-6 col-lg-6">
+          <p>Enter Room No.</p>
+          <input type="text" class="form-control" v-model="roominput" style="text-align: center">
+          <div class="row justify-content-center">
+            <button class="btn btn-primary btn-sm mt-2 mr-1" @click.prevent="createRoom">Create Room</button>
+            <button class="btn btn-success btn-sm mt-2" @click.prevent="joinRoom">Join Room</button>
+          </div>
+        </div>  
+      </div>
+    </div>
+
+    <div class="container mt-5" style="min-height: 80vh; max-height: 80vh; overflow-y: auto" v-else>
       <div class="col-12 col-sm-12 col-md-12 col-lg-12 mr-2 msg_history">
         <div v-for="(message, i) in messages" :key="i">
           <div class="row mt-2 ml-1" v-if="message.email != authUser.email">
@@ -53,13 +66,54 @@ export default {
       message: '',
       messages: [],
       authUser: {},
+      roomNo: '',
+      roominput: '',
     };
   },
   methods: {
+    rndRoomNo(){
+      var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+      var string_length = 8;
+      var randomstring = '';
+      for (var i=0; i<string_length; i++) {
+        var rnum = Math.floor(Math.random() * chars.length);
+        randomstring += chars.substring(rnum,rnum+1);
+      }
+      this.roomNo = randomstring
+      alert('Room No: ' + this.roomNo)
+      this.fetchMessage();
+    },
+    createRoom(){
+      this.rndRoomNo()
+      db.collection("rooms").add({
+        roomNumber: this.roomNo,
+        user: this.authUser['displayName'],
+        email: this.authUser['email'],
+        photoURL: this.authUser['photoURL'],
+      })
+    },
+    async joinRoom(){
+      if(this.roominput.trim() == '')
+      return alert('Room number cant be null')
+
+      // Create a reference to the cities collection
+      const chatroom = db.collection('rooms');
+      const room_number = await chatroom.where('roomNumber', '==', this.roominput).get();
+
+      if (room_number.empty) {
+        console.log('No matching documents.');
+        return;
+      }  
+
+      this.roomNo = this.roominput
+      this.fetchMessage();
+
+    },
     scrollToBottom() {
       let box = document.querySelector('.container');
       box.scrollTop = box.scrollHeight;
     },
+
     saveMessage() {
       var Filter = require('bad-words'),
       filter = new Filter();
@@ -84,7 +138,8 @@ export default {
         datetime: new Date(),
         user: this.authUser['displayName'],
         email: this.authUser['email'],
-        photoURL: this.authUser['photoURL']
+        photoURL: this.authUser['photoURL'],
+        room: this.roomNo
       }).then(()=>{
         this.scrollToBottom()
       });
@@ -93,8 +148,7 @@ export default {
     },
     fetchMessage() {
       //retrieve firestore collection automatically
-
-      db.collection("chat").orderBy('datetime', 'asc').onSnapshot((snapshot) => {
+      db.collection("chat").where('room', '==', this.roomNo).orderBy('datetime', 'asc').onSnapshot((snapshot) => {
         let allMessages = [];
         snapshot.forEach((chat) => {
           allMessages.push(chat.data());
@@ -103,22 +157,20 @@ export default {
         this.messages = allMessages;
         this.scrollToBottom()
       });
-    
+      
     },
   },
   created() {
-    this.fetchMessage();
-    $('#welcomeModal').modal('show')
     firebase.auth().onAuthStateChanged(user=>{
         if(user){
           this.authUser = user
-          console.log(this.authUser)
         }
         else
         {
           this.authUser ={}
         }
     })
+    console.log(this.messages)
   },
   components: {},
   beforeRouteEnter (to, from, next) {
@@ -149,5 +201,8 @@ export default {
   max-width: 80%;
   margin-left: 50%;
   transform: translateX(-50%)
+}
+p {
+  margin-bottom: 0 !important;
 }
 </style>
